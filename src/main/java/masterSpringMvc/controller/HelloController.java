1a421b093ff07b86ccb5e936336260ed5b2f0758
CopySpotter <krejcar@gmx.net>
2016-04-07 17:08:27 +0200

    buggers' commit

--- src/main/java/masterSpringMvc/controller/HelloController.java
+++ src/main/java/masterSpringMvc/controller/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 @RequestMapping("/")
 @ResponseBody
 public String hello() {
-return "Hello you, world from HelloController!";
+return "Hello you bugger!! ...,.... world from HelloController!";
 }
 }
