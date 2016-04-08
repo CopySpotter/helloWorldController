8dc1c1346f472079c8e2b0754f25605172cb07a0
CopySpotter <krejcar@gmx.net>
2016-04-08 10:53:09 +0200

    C:/Program Files/Git/2 als requested mapping eingefügt und / wieder ermöglicht

--- src/main/java/masterSpringMvc/controller/HelloController.java
+++ src/main/java/masterSpringMvc/controller/HelloController.java
@@ -4,10 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
-public class HelloController {
-@RequestMapping("/1")
-@ResponseBody
-public String hello() {
-return "Hello you bugger!! ...,.... world from HelloController!";
-}
+public class HelloController 
+{
+    @RequestMapping("/") // Aufruf mit http://localhost:8080
+    @RequestMapping("/2") // Aufruf mit http://localhost:8080/2
+	@RequestMapping("/1") // Aufruf mit http://localhost:8080/1
+	@ResponseBody
+	public String hello() 
+	{
+	  return "Hello you bugger!! ...,.... world from HelloController!";
+    }
 }
