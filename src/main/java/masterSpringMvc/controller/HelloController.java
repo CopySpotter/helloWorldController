1f294d68b8c2fc000c59a645d6b725996a2a26d5
CopySpotter <krejcar@gmx.net>
2016-04-08 10:46:55 +0200

    C:/Program Files/Git/1 als requested mapping eingefügt

--- src/main/java/masterSpringMvc/controller/HelloController.java
+++ src/main/java/masterSpringMvc/controller/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloController {
-@RequestMapping("/")
+@RequestMapping("/1")
 @ResponseBody
 public String hello() {
 return "Hello you bugger!! ...,.... world from HelloController!";
