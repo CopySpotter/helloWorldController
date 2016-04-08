beac7e55aae210ee1983ae676c4cf17bd3a8253a
CopySpotter <krejcar@gmx.net>
2016-04-08 11:01:08 +0200

    C:/Program Files/Git/2 als requested mapping eingefügt, / wieder ermöglichen wird nicht kompiliert - nur ein (1) einsprungspunkt

--- src/main/java/masterSpringMvc/controller/HelloController.java
+++ src/main/java/masterSpringMvc/controller/HelloController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloController 
 {
-    @RequestMapping("/") // Aufruf mit http://localhost:8080
+    //@RequestMapping("/")  // Aufruf mit http://localhost:8080
     @RequestMapping("/2") // Aufruf mit http://localhost:8080/2
-	@RequestMapping("/1") // Aufruf mit http://localhost:8080/1
+	//@RequestMapping("/1") // Aufruf mit http://localhost:8080/1
 	@ResponseBody
 	public String hello() 
 	{
